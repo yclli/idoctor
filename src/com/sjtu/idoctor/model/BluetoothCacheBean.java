9960f01562db7c7918eb491289d3447f35ee741e
yclli <yclli01@126.com>
2015-07-19 17:13:46 +0800

    final version

--- src/com/sjtu/idoctor/model/BluetoothCacheBean.java
+++
@@ -1,7 +0,0 @@
-package com.sjtu.idoctor.model;
-
-public class BluetoothCacheBean {
-	public static String BlueToothAddress = null;
-	public static String lastblueToothAddress = null;
-	public static boolean isOpen = false;
-}
